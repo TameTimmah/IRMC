8f1d0961a72f7bdb7ffcde2f55eec042425a98f4
BranicYeti <branicyeti@live.co.uk>
2013-01-18 22:30:58 +0000

    Lets see what damage this can cause...

--- src/main/java/com/notoriousdev/irmc/IRMC.java
+++ src/main/java/com/notoriousdev/irmc/IRMC.java
@@ -1,6 +1,20 @@
 package com.notoriousdev.irmc;
 
+import com.notoriousdev.irmc.irc.Bot;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class IRMC extends JavaPlugin {
+
+    private Bot bot;
+
+    @Override
+    public void onDisable() {
+        // TODO: We need this shit, yo!
+    }
+
+    @Override
+    public void onEnable() {
+        bot = new Bot(this);
+        bot.startHere();
+    }
 }
---
+++ src/main/java/com/notoriousdev/irmc/irc/Bot.java
@@ -0,0 +1,33 @@
+package com.notoriousdev.irmc.irc;
+
+import com.notoriousdev.irmc.IRMC;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.pircbotx.PircBotX;
+import org.pircbotx.exception.IrcException;
+
+public class Bot {
+    
+    private final PircBotX bot;
+    private final IRMC plugin;
+    
+    public Bot(IRMC plugin) {
+        this.plugin = plugin;
+        bot = new PircBotX();
+    }
+    
+    public void startHere() {
+        // Lets test some stuff, shall we?
+        bot.setName("Professor`Oak");
+        try {
+            bot.connect("irc.esper.net");
+        } catch (IOException ex) {
+            Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IrcException ex) {
+            Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        bot.joinChannel("#thereverend403");
+        bot.getChannel("#thereverend403").sendMessage("Connection was a success!");
+    }
+}
