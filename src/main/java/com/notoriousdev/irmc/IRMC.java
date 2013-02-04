9546fde665a7860f27609070197bcec59ce837b1
BranicYeti <branicyeti@live.co.uk>
2013-02-04 17:00:09 +0000

    Start over.

--- src/main/java/com/notoriousdev/irmc/IRMC.java
+++ src/main/java/com/notoriousdev/irmc/IRMC.java
@@ -4,32 +4,4 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class IRMC extends JavaPlugin
 {
-
-    private Bot bot;
-    //private final int tid;
-
-    @Override
-    public void onDisable()
-    {
-        bot.kill();
-        bot = null;
-    }
-
-    @Override
-    public void onEnable()
-    {
-        saveDefaultConfig();
-        System.out.println("[IRMC] Initialising Bot...");
-        getServer().getScheduler().runTaskAsynchronously(this, bot);
-        registerEvents();
-        registerCommands();
-    }
-
-    private void registerEvents()
-    {
-    }
-
-    private void registerCommands()
-    {
-    }
 }
