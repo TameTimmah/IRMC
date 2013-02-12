bb414640c39e5b9a33113ebf7106bb2db9aeecbe
Lee Watson <synystershadows403@googlemail.com>
2013-02-12 12:26:11 +0000

    Update src/main/java/com/notoriousdev/irmc/Configuration.java
    Bad language from Yeti fixed ;)

--- src/main/java/com/notoriousdev/irmc/Configuration.java
+++ src/main/java/com/notoriousdev/irmc/Configuration.java
@@ -2,7 +2,9 @@ package com.notoriousdev.irmc;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -10,6 +12,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 public class Configuration
 {
 
+    private Map<String, String> paths = new HashMap<String, String>();
     private final IRMC plugin;
     private final FileConfiguration config;
     /* General Config Options */
@@ -68,6 +71,28 @@ public class Configuration
     {
         this.plugin = plugin;
         this.config = plugin.getConfig();
+        /* Potentially being moved. Do whatever you want with it. I'm just documenting it. */
+        paths.put("debug", "debug");
+        paths.put("verbose", "irc.verbose");
+        paths.put("autonick", "irc.autonick");
+        paths.put("autorejoin", "irc.autorejoin");
+        paths.put("autoreconnect", "irc.autoreconnect");
+        paths.put("autosplit", "irc.autosplit");
+        paths.put("address", "irc.server.address");
+        paths.put("port", "irc.server.port");
+        paths.put("serverpass", "irc.server.password");
+        paths.put("usessl", "irc.server.use-ssl");
+        paths.put("verifyssl", "irc.server.verify-ssl");
+        paths.put("nickname", "irc.identity.nickname");
+        paths.put("ident", "irc.identity.ident");
+        paths.put("password", "irc.identity.password");
+        paths.put("channels", "irc.channels");
+        paths.put("trigger", "irc.trigger");
+    }
+
+    public void loadGeneralConfig()
+    {
+        this.debug = config.getBoolean("irc.debug");
     }
 
     public void loadBukkitConfig()
@@ -100,6 +125,7 @@ public class Configuration
 
     public void loadConfig()
     {
+        loadGeneralConfig();
         loadBukkitConfig();
         loadIrcConfig();
     }
@@ -123,9 +149,18 @@ public class Configuration
         return String.valueOf(port);
     }
 
-    public void setConfig(String path, Object setting)
+    public String getPath(String setting)
     {
-        config.set(path, setting);
+        return paths.get(setting);
+    }
+
+    public void setConfig(String cfg, Object setting)
+    {
+        if (!config.contains(cfg)) {
+            plugin.getLogger().warning("U DUN GOOFED SON!");
+            return;
+        }
+        config.set(getPath(cfg), setting);
         try {
             config.save(new File(plugin.getDataFolder(), "config.yml"));
         } catch (IOException ex) {
