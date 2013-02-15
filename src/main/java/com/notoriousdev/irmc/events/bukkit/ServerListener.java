10e94fff08e6696be9c33292ba9182b85f1a9d83
BranicYeti <branicyeti@live.co.uk>
2013-02-15 19:13:04 +0000

    Rough join, quit and kick relay

--- src/main/java/com/notoriousdev/irmc/events/bukkit/ServerListener.java
+++ src/main/java/com/notoriousdev/irmc/events/bukkit/ServerListener.java
@@ -1,5 +1,6 @@
 package com.notoriousdev.irmc.events.bukkit;
 
+import com.notoriousdev.irmc.Bot;
 import com.notoriousdev.irmc.IRMC;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,24 +13,29 @@ public class ServerListener implements Listener
 {
 
     private final IRMC plugin;
+    private final Bot bot;
 
-    public ServerListener(IRMC plugin)
+    public ServerListener(IRMC plugin, Bot bot)
     {
         this.plugin = plugin;
+        this.bot = bot;
     }
 
     @EventHandler
     public void playerJoin(PlayerJoinEvent event)
     {
+        bot.relayServerMessage(String.format("{%s has joined the server!}", event.getPlayer().getName()));
     }
 
     @EventHandler
     public void playerQuit(PlayerQuitEvent event)
     {
+        bot.relayServerMessage(String.format("{%s has left the server!}", event.getPlayer().getName()));
     }
 
     @EventHandler
     public void playerKicked(PlayerKickEvent event)
     {
+        bot.relayServerMessage(String.format("{%s has been kicked for reason: %s}", event.getPlayer().getName(), event.getReason()));
     }
 }
